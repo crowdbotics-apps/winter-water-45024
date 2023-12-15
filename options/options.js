c16a69351ad8ae7b72d8e7a659cad0d30218bfe0
Crowdbotics <team@crowdbotics.com>
2023-12-15 07:57:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "winter-water-45024",
+  url: "https://winter-water-45024.botics.co",
+  api: "https://winter-water-45024.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
